4c2c0b60788206d01a6c8b73d5ddb0f07650bf63
Gustavo Coutinho <gustavolgcr@gmail.com>
2018-09-18 23:46:03 -0300

    Refatorando a requisição para criação do grafo

---
+++ api/src/main/java/com/graphast/api/repository/GraphRepo.java
@@ -0,0 +1,44 @@
+package com.graphast.api.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.insightlab.graphast.model.Graph;
+
+public class GraphRepo {
+	
+	Map<String, Graph> map = new HashMap<>();
+
+	private static GraphRepo graphRepo;
+
+	private GraphRepo() {
+
+	}
+
+	public static GraphRepo getInstance() {
+		if (graphRepo == null) {
+			graphRepo = new GraphRepo();
+		}
+
+		return graphRepo;
+	}
+
+	public void create(String graphName) {
+		Graph graph = new Graph();
+		map.put(graphName, graph);
+	}
+
+	public void update(String graphName, Graph graph) {
+		if (map.containsKey(graphName)) {
+			map.put(graphName, graph);
+		}
+	}
+
+	public void delete(String name) {
+		map.remove(name);
+	}
+
+	public Graph get(String name) {
+		return map.get(name);
+	}
+}
--- api/src/main/java/com/graphast/api/resource/GraphResource.java
+++ api/src/main/java/com/graphast/api/resource/GraphResource.java
@@ -1,28 +1,24 @@
 package com.graphast.api.resource;
 
 import org.insightlab.graphast.model.Graph;
-import org.insightlab.graphast.model.Node;
-import org.insightlab.graphast.model.components.spatial_components.Point;
-import org.insightlab.graphast.model.components.spatial_components.SpatialNodeComponent;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.graphast.api.repository.GraphRepo;
 
 @RestController
 @RequestMapping("/graph-generator")
 public class GraphResource {
 	
 	@PostMapping
-	public Node createGrah(@RequestBody Point point){
-		Graph graph = new Graph();
-
-		SpatialNodeComponent snc = new SpatialNodeComponent(point);
-		Node n1 = new Node(01);
-		n1.addComponent(snc);
+	public Graph createGraph(@RequestBody String graphName){
 		
-		graph.addNode(n1);
-		return n1;
+		GraphRepo.getInstance().create(graphName);
+
+		return GraphRepo.getInstance().get(graphName);
+
 	}
 
 }
